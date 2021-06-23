e42032860f4a5365cef3a8614d6f4e5b231e38d8
adiwijaya <adiwijaya25@gmail.com>
2021-06-23 21:05:47 +0800

    Chapter 4 commit

--- chapter-3/code/bigquery_scenario_2_step_1b_load_regions_data.py
+++ chapter-3/code/bigquery_scenario_2_step_1b_load_regions_data.py
@@ -7,12 +7,19 @@ target_table_id = "{}.raw_bikesharing.regions".format(project_id)
 
 def load_data_from_bigquery_public(public_table_id, target_table_id):
     client = bigquery.Client()
-    job_config = bigquery.QueryJobConfig(destination=target_table_id)
+    job_config = bigquery.QueryJobConfig(
+    destination = starget_table_id,
+    write_disposition ='WRITE_TRUNCATE')
 
     table = bigquery.Table(target_table_id)
     sql = f"""SELECT * FROM `{public_table_id}`;"""
 
     query_job = client.query(sql, job_config=job_config)
-    query_job.result()
+    
+    try:
+        query_job.result()
+        print("Query success")
+    except Exception as e:
+            print(e)
 
 load_data_from_bigquery_public(public_table_id, target_table_id)
--- chapter-3/code/gcloud_export_cloudsql_to_gcs.sh
+++ chapter-3/code/gcloud_export_cloudsql_to_gcs.sh
@@ -10,4 +10,4 @@ gcloud sql export csv mysql-instance-source \
 gs://$bucket_name/mysql_export/stations/20180102/stations.csv \
 --database=apps_db \
 --offload \
---query='SELECT * FROM stations WHERE station_id <=400;'
+--query='SELECT * FROM stations WHERE station_id;'
